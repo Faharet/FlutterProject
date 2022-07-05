c4e7988ecd12debce12d89a06087e56693ea5fa6
user2 <smart.ass.raty@gmail.com>
2022-07-05 18:16:22 +0600

    have to check on x64 mingw

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool diskpartOnline = false;
-  bool acronisOnline = false;
-  String log = 'This is log file';
+  String log = "This is a log file";
   List<String> elements = [];
   @override
   Widget build(BuildContext context) {
@@ -40,52 +38,68 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context, 
+                builder: (BuildContext context){
+                  return AlertDialog(
+                    title: Text(Directory.current.path),
+                  );
+                }
+              );
+            },
+            child: const Text('BatEventByProcess'),
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context, 
-                    builder: (BuildContext context){
-                      return AlertDialog(
-                        title: Text(Directory.current.path),
-                      );
-                    }
-                  );
-                },
-                child: const Text('BatEventByProcess'),
-              ),
-            ]
-          ),
-          Center(
-            child: TimerBuilder.periodic(
-              const Duration(seconds: 1), 
-              builder: (context){
-                diskpartStatus();
-                return Column(
+          TimerBuilder.periodic(
+            const Duration(seconds: 1), 
+            builder: (context){
+              log = pm.diskpartStatus();
+              return Center(
+                child: Column(
                   children: <Widget>[
                     GridView.builder(
+                      scrollDirection: Axis.vertical,
                       shrinkWrap: true,
                       itemCount: elements.length,
                       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 35.0, 
-                        crossAxisSpacing: 20.0, 
-                        mainAxisSpacing: 20.0),
+                        maxCrossAxisExtent: 55.0,
+                        mainAxisSpacing: 5.0,
+                        crossAxisSpacing: 5.0
+                      ),
                       itemBuilder: (context, i){
-                        return Card(
-                          
-                          child: OutlinedButton(
-                            onPressed: () => print(elements[i]), 
-                            child: const SizedBox(
-                              child: Text("Button"),
-                              width: 20.0,
-                              height: 20.0,
-                            ),
-                          ),
+                        return Center(
+                          child: ElevatedButton(
+                            onPressed: (){
+                              int result = pm.eject("\\\\.\\H:");
+                              if(result == 0){
+                                showDialog(
+                                  context: context, 
+                                  builder: (BuildContext context){
+                                    return AlertDialog(
+                                      title: Text("Ejected $result"),
+                                    );
+                                  }
+                                );
+                              }
+                              else{
+                                showDialog(
+                                  context: context, 
+                                  builder: (BuildContext context){
+                                    return AlertDialog(
+                                      title: Text("Error $result"),
+                                    );
+                                  }
+                                );
+                              }
+                            }, 
+                            child: Text(elements[i]),
+                          )
                         );
                       }
                     ),
@@ -96,33 +110,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ]
-                );
-              },
-            )
-          ),
+                )
+              );
+            },
+          )
         ],
       ),
     );
-  }
-
-  void diskpartStatus(){
-    acronisOnline = pm.findProc();
-    if(acronisOnline && !diskpartOnline){
-      Process.run("mspaint.exe", []);
-      diskpartOnline = true;
-      log = "Acronis online, Diskpart offline";
-    }
-    if(acronisOnline && diskpartOnline){
-      diskpartOnline = true;
-      log = "Acronis online, Diskpart online";
-    }
-    if(!acronisOnline && diskpartOnline){
-      Process.run("calc.exe", []);
-      diskpartOnline = false;
-      log = "Acronis offline, Diskpart offline";
-    }
-    if(!acronisOnline && !diskpartOnline){
-      log = "Acronis offline, Diskpart offline";
-    }
   }
 }
--- lib/process_monitor.dart
+++ lib/process_monitor.dart
@@ -1,5 +1,5 @@
 import 'dart:ffi' as ffi;
-import 'dart:io' show Directory;
+import 'dart:io' show Directory, Process;
 import 'package:path/path.dart' as path;
 import 'package:ffi/ffi.dart' as pffi;
 
@@ -24,14 +24,54 @@ String getDisks(){
   return buffer.toDartString();
 }
 
+typedef MediaNative = ffi.Int Function(ffi.Pointer<pffi.Utf8> media);
+typedef Media = int Function(ffi.Pointer<pffi.Utf8> media);
+
+final Media ejectMedia = cppLibsDll.lookupFunction<MediaNative, Media>('ejectMedia');
+int eject(String media){
+  var buffer = ejectMedia(media.toNativeUtf8());
+  return buffer;
+}
+
+final Media loadMedia = cppLibsDll.lookupFunction<MediaNative, Media>('loadMedia');
+int load(String media){
+  var buffer = loadMedia(media.toNativeUtf8());
+  return buffer;
+}
+
 List<String> getButtons(){
   String diskpartlog = "This is a diskpart log";
   diskpartlog = getDisks();
   final List<String> elements = [];
   int i = 0;
   while(i < diskpartlog.length){
-    elements.add(diskpartlog[i]);
+    elements.add(diskpartlog[i] + diskpartlog[i+1] + diskpartlog[i+2]);
     i = i + 3;
   }
   return elements;
 }
+
+String diskpartStatus(){
+    String log = "This is a log file";
+    bool diskpartOnline = false;
+    bool acronisOnline = false;
+    acronisOnline = findProc();
+    if(acronisOnline && !diskpartOnline){
+      Process.run("mspaint.exe", []);
+      diskpartOnline = true;
+      log = "Acronis online, Diskpart offline";
+    }
+    if(acronisOnline && diskpartOnline){
+      diskpartOnline = true;
+      log = "Acronis online, Diskpart online";
+    }
+    if(!acronisOnline && diskpartOnline){
+      Process.run("calc.exe", []);
+      diskpartOnline = false;
+      log = "Acronis offline, Diskpart offline";
+    }
+    if(!acronisOnline && !diskpartOnline){
+      log = "Acronis offline, Diskpart offline";
+    }
+    return log;    
+  }
