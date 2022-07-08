112552fbd7db0f58e286735b0fc3e9a77ce93e2f
user2 <smart.ass.raty@gmail.com>
2022-07-08 11:41:52 +0600

    classes have changed widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:timer_builder/timer_builder.dart';
-import 'view_controller.dart' as vc;
-import 'drive_controller.dart' as dc;
+import 'controller.dart' as c;
+
+late String drive;
+bool route = false;
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +11,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,14 +24,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  static late String button;
+class _MyHomePageState extends State<MyHomePage>{
+  c.Controller controller = c.Controller();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +40,10 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: <Widget>[
           ElevatedButton(
             onPressed: () {
+              setState((){
+                controller.elements.clear();
+                route = false;
+              });
               showDialog(
                 context: context, 
                 builder: (BuildContext context){
@@ -52,48 +57,59 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          TimerBuilder.periodic(
-            const Duration(seconds: 1), 
-            builder: (context){
-              vc.ViewController viewController = vc.ViewController();
-              return Center(
-                child: Column(
-                  children: <Widget>[
-                    GridView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: viewController.elements.length,
-                      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                        maxCrossAxisExtent: 55.0,
-                        mainAxisSpacing: 5.0,
-                        crossAxisSpacing: 5.0
-                      ),
-                      itemBuilder: (context, i){
-                        return Center(
-                          child: ElevatedButton(
-                            onPressed: (){
-                              button = viewController.elements[i];
-                              Navigator.push(
-                                context, 
-                                MaterialPageRoute<void>(
-                                  builder: (BuildContext context) => dc.DriveController(b: button),
-                                ),
-                              );
-                            }, 
-                            child: Text(viewController.elements[i]),
-                          )
-                        );
-                      }
-                    ),
-                  ]
-                )
-              );
-            },
-          )
-        ],
+      body: Center(
+        child: route ? driveController(drive) : viewController(),
       ),
+    );
+  }
+
+  Widget viewController(){
+    controller.getButtons();
+    return TimerBuilder.periodic(
+      const Duration(seconds: 1), 
+      builder: (context) {
+        return Center(
+          child: Column(
+            children: <Widget>[
+              GridView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: controller.elements.length,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 55.0,
+                  mainAxisSpacing: 5.0,
+                  crossAxisSpacing: 5.0
+                ),
+                itemBuilder: (context, i){
+                  return Center(
+                    child: ElevatedButton(
+                      onPressed: (){
+                        setState(() {
+                          drive = controller.elements[i];
+                          route = true;
+                        });
+                      }, 
+                      child: Text(controller.elements[i]),
+                    )
+                  );
+                }
+              ),
+            ]
+          )
+        ); 
+      },
+    );
+  }
+
+  Widget driveController(String button){
+    return TimerBuilder.periodic(
+      const Duration(seconds: 1), 
+      builder: (context) {
+        controller.getProcessLog(button);
+        return Center(
+          child: Text(controller.processLog)
+        ); 
+      },
     );
   }
 }
